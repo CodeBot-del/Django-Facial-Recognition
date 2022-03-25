99e999faf8c80c3c90d16ee3ed7915745d8ba351
gracie2 <stevenmanangu360@gmail.com>
2022-03-25 13:27:25 +0300

    Successfully rendered opencv image to browser

--- scanner/views.py
+++ scanner/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 import cv2
 import base64
+from io import BytesIO
+from PIL import Image
 
 
 # Create your views here.
@@ -12,14 +14,18 @@ def scan(request):
     mode = request.POST['mode']
     message = mode
     
-    img = "/home/egovridc/Desktop/Steve/DjangoTutorial/alpha/scanner/cena.jpeg" 
+    img = "/home/egovridc/Desktop/cena.jpeg" 
     frame = cv2.imread(img)
     _, frame_buff = cv2.imencode('.jpg', frame) 
-    frame_b64 = base64.b64encode(frame_buff)
+    im_bytes = frame_buff.tobytes()
+    frame_b64 = base64.b64encode(im_bytes)
+    new_img = frame_b64.decode()
     
     
     
     
-    return render(request, 'scan.html', {"message":message, 'img': frame_b64})
+    
+    
+    return render(request, 'scan.html', {"message":message, "img": new_img})
 
 #to capture video class
